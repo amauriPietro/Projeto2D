2b5e091fa30d9290563269d3527e479861d5a7bb
esquilor <amauripietropaolo@gmail.com>
2018-06-04 11:13:32 -0300

    tarefa 1

---
+++ Assets/scripts/MainMenuUIctrl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIctrl : MonoBehaviour {
+
+	public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
