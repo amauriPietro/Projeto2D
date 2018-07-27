ea6a0f2412dbcb13a1f26542579f8842eab4cc9b
Amauri Pietropaolo <amauripietropaolo@gmail.com>
2018-07-27 08:43:36 -0300

    singleJump

--- Assets/scripts/PlayerCtrl.cs
+++ Assets/scripts/PlayerCtrl.cs
@@ -6,11 +6,17 @@ public class PlayerCtrl : MonoBehaviour {
 
 	public float horizontalSpeed = 5f;
 	public float jumpSpeed = 600f;
+	public float feetWidth = 0.5f;
+	public float feetHeight = 0.1f;
+	public bool isGrounded;
+	public LayerMask whatIsGround;
+	
 
 	Rigidbody2D rb;
 	SpriteRenderer sr;
 	Animator anim;
 	bool isJumping = false;
+	public Transform feet;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -18,9 +24,12 @@ public class PlayerCtrl : MonoBehaviour {
 		anim = GetComponent<Animator>();
 	}
 	
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube(feet.position, new Vector3(feetWidth, feetHeight,  0f));
+	}
 	// Update is called once per frame
 	void Update () {
-		
+		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y), new Vector2(feetWidth, feetHeight), 360f, whatIsGround);
 		float horizontalInput = Input.GetAxisRaw("Horizontal");
 		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
 		if(horizontalPlayerSpeed != 0){
@@ -59,9 +68,11 @@ public class PlayerCtrl : MonoBehaviour {
 		}
 	}
 	void jump(){
-		isJumping = true;
-		rb.AddForce(new Vector2(0f, jumpSpeed));
-		anim.SetInteger("State", 1);
+		if(isGrounded){
+			isJumping = true;
+			rb.AddForce(new Vector2(0f, jumpSpeed));
+			anim.SetInteger("State", 1);
+		}
 	}
 	void OnCollisionEnter2D(Collision2D other){
 		if(other.gameObject.layer == LayerMask.NameToLayer("Ground")){
