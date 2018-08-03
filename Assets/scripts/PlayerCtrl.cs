a679fb5004dc69a3d1847a208d50e5c511a1a791
Amauri Pietropaolo <amauripietropaolo@gmail.com>
2018-08-03 08:53:37 -0300

    moeda no player

--- Assets/scripts/PlayerCtrl.cs
+++ Assets/scripts/PlayerCtrl.cs
@@ -94,4 +94,11 @@ public class PlayerCtrl : MonoBehaviour {
 			isJumping = false;
 		}
 	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("coin"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
