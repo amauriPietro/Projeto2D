87000b74d2cddf531ed019cbd68aafd6239113a6
Amauri Pietropaolo <amauripietropaolo@gmail.com>
2018-08-14 10:28:03 -0300

    MovingPlatforms2 + HUD

---
+++ Assets/scripts/feetCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feetCtrl : MonoBehaviour {
+
+    GameObject player;
+
+	void Start () {
+        player = transform.parent.gameObject;
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("MovingPlatform"))
+        {
+            player.transform.parent = other.transform.parent.transform;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("MovingPlatform"))
+        {
+            player.transform.parent = null;
+        }
+    }
+}
