b46c161835330575cf0565150333f9d73cb74099
Amauri Pietropaolo <amauripietropaolo@gmail.com>
2018-08-24 18:07:36 -0300

    Flip + idle monster patrol

---
+++ Assets/scripts/MonsterPatrolCtrl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterPatrolCtrl : MonoBehaviour
+{
+
+    public Transform pos1, pos2;
+    public float speed = 2f;
+    public float waitTime = 0.5f;
+    Vector3 nextPos;
+    Animator anim;
+    SpriteRenderer sr;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        nextPos = pos1.position;
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            if (transform.position == pos1.position)
+            {
+                nextPos = pos2.position;
+                anim.SetInteger("state", 1);
+                yield return new WaitForSeconds(waitTime);
+                anim.SetInteger("state", 0);
+                sr.flipX = !sr.flipX;
+            }
+            if (transform.position == pos2.position)
+            {
+                nextPos = pos1.position;
+                anim.SetInteger("state", 1);
+                yield return new WaitForSeconds(waitTime);
+                anim.SetInteger("state", 0);
+                sr.flipX = !sr.flipX;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
+    }
+
+}
